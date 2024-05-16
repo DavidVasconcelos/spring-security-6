034003bf04d13148e3475d35e93b7b41f43fae0e
David Santos Vasconcelos <dsv10@hotmail.com>
2024-05-16 16:45:51 -0300

    -  cors configuration

--- section6/springsecuritysection6/src/main/java/com/udemy/springsecuritysection6/config/SecurityConfig.java
+++ section6/springsecuritysection6/src/main/java/com/udemy/springsecuritysection6/config/SecurityConfig.java
@@ -1,13 +1,18 @@
 package com.udemy.springsecuritysection6.config;
 
+import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -22,6 +27,7 @@ public class SecurityConfig {
             .requestMatchers("/notices", "/contact", "/register").permitAll()
         )
         .csrf(AbstractHttpConfigurer::disable)
+        .cors(Customizer.withDefaults())
         .formLogin(withDefaults())
         .httpBasic(withDefaults());
     return http.build();
@@ -39,5 +45,18 @@ public class SecurityConfig {
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public CorsFilter corsFilter() {
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    final CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+    config.setAllowedMethods(Collections.singletonList("*"));
+    config.setAllowCredentials(true);
+    config.setAllowedHeaders(Collections.singletonList("*"));
+    config.setMaxAge(3600L);
+    source.registerCorsConfiguration("/**", config);
+    return new CorsFilter(source);
   }
 }
