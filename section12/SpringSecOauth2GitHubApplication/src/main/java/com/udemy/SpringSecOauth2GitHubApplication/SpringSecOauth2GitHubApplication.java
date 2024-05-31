d1c005ce692b2066270c84363fa974612a1ae7ff
David Santos Vasconcelos <dsv10@hotmail.com>
2024-05-31 16:27:50 -0300

    - spring oauth 2 init

---
+++ section12/SpringSecOauth2GitHubApplication/src/main/java/com/udemy/SpringSecOauth2GitHubApplication/SpringSecOauth2GitHubApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.SpringSecOauth2GitHubApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecOauth2GitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecOauth2GitHubApplication.class, args);
+	}
+
+}
---
+++ section12/SpringSecOauth2GitHubApplication/src/test/java/com/udemy/SpringSecOauth2GitHubApplication/SpringSecOauth2GitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.SpringSecOauth2GitHubApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecOauth2GitHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
